e7f46e03d387a68a3d15c56950d56d0db75a0629
mcc <andi.m.mcclure@gmail.com>
2024-08-26 22:41:11 -0400

    "Version zero": finish() on launch, theoretically blank icon (icon doesn't work)

--- app/src/main/java/technology/dryad/zero/MainActivity.kt
+++ app/src/main/java/technology/dryad/zero/MainActivity.kt
@@ -16,32 +16,6 @@ import technology.dryad.zero.ui.theme.ZeroTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            ZeroTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ZeroTheme {
-        Greeting("Android")
+        finish();
     }
 }
