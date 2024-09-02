289d73a006d1f99e5770f12e6b09ba92a8a6fb38
mike castleman <m@mlcastle.net>
2024-09-02 00:07:24 -0400

    remove most dependencies

--- app/src/main/java/technology/dryad/zero/MainActivity.kt
+++ app/src/main/java/technology/dryad/zero/MainActivity.kt
@@ -1,19 +1,9 @@
 package technology.dryad.zero
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import technology.dryad.zero.ui.theme.ZeroTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         finish();
